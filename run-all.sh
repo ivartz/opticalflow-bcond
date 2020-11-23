503633fd28e4f80962f8e052b5e4f2468471ed8e
Ivar T Hovden <djloek@gmail.com>
2020-11-23 21:49:38 +0100

    final collect script

--- run-all.sh
+++ run-all.sh
@@ -6,14 +6,14 @@ dataset="/mnt/HDD3TB/derivatives/cancer-sim-SAILOR_PROCESSED_MNI-01"
 #bash of-ilk-models-dataset.sh
 #bash of-tvl1-models-dataset.sh
 
-analyse-of-farneback-dataset.sh
-analyse-of-ilk-dataset.sh
-analyse-of-tvl1-dataset.sh
+#analyse-of-farneback-dataset.sh
+#analyse-of-ilk-dataset.sh
+#analyse-of-tvl1-dataset.sh
 
-bash collect-of-farneback-estimation-error-segments.sh 0 > $dataset/syn-of-farneback-estimation-error-segments.txt
-bash collect-of-ilk-estimation-error-segments.sh 0 > $dataset/syn-of-ilk-estimation-error-segments.txt
-bash collect-of-tvl1-estimation-error-segments.sh 0 > $dataset/syn-of-tvl1-estimation-error-segments.txt
+bash collect-of-farneback-estimation-error-segments.sh 0 > $dataset/of-farneback-estimation-error-segments.txt
+bash collect-of-ilk-estimation-error-segments.sh 0 > $dataset/of-ilk-estimation-error-segments.txt
+bash collect-of-tvl1-estimation-error-segments.sh 0 > $dataset/of-tvl1-estimation-error-segments.txt
 
-bash collect-of-farneback-estimation-error-segments.sh 1 > $dataset/syn-of-farneback-estimation-error-relative-segments.txt
-bash collect-of-ilk-estimation-error-segments.sh 1 > $dataset/syn-of-ilk-estimation-error-relative-segments.txt
-bash collect-of-tvl1-estimation-error-segments.sh 1 > $dataset/syn-of-tvl1-estimation-error-relative-segments.txt
+bash collect-of-farneback-estimation-error-segments.sh 1 > $dataset/of-farneback-estimation-error-relative-segments.txt
+bash collect-of-ilk-estimation-error-segments.sh 1 > $dataset/of-ilk-estimation-error-relative-segments.txt
+bash collect-of-tvl1-estimation-error-segments.sh 1 > $dataset/of-tvl1-estimation-error-relative-segments.txt
